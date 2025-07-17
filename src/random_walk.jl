302caa3bd2ba9e56eed7cf3ad6a5c2a4cf62e049
MK <kurihara.m.038@ms.saitama-u.ac.jp>
2025-07-17 15:36:28 +0900

    Update project files

--- src/random_walk.jl
+++ src/random_walk.jl
@@ -3,21 +3,22 @@ module random_walk
 # ここではCubaを使う。
 using Cuba
 
-# (3 / (2 π)^3 ) * ∫_-π^π ∫_-π^π ∫_-π^π 1 / (3 - cos(x) - cos(y) - cos(z)) dx dy dzを計算したい
-# まずは、∫_-π^π ∫_-π^π ∫_-π^π 1 / (3 - cos(x) - cos(y) - cos(z)) dx dy dzを計算したい
+# u(3) ≡ (3 / (2 π)^3 ) * ∫_-π^π ∫_-π^π ∫_-π^π 1 / (3 - cos(x) - cos(y) - cos(z)) dx dy dzを計算したい
+# まずは、∫_[-π, π]^3 1 / (3 - cos(x) - cos(y) - cos(z)) dx dy dzを計算したい
 function u3_cuba()
     function integrand(x)
         # Cubaの積分区間は[0,1]^3なので、変数変換をして積分区間を対応させたい
         # [0,1]^3 → [-π, π]^3 に変換
         θ = 2π .* x .- π
+        # 原点で発散があるので、原点近傍では0にする
         denom = 3 - sum(cos.(θ))
         if abs(denom) < 1e-12 || !isfinite(denom)
             return 0.0
         else
-            return (2π)^3 / denom  # ヤコビアン補正も忘れずに！
+            return (2π)^3 / denom  # ヤコビアン補正は(2π)^3
         end
     end
-
+    # u(3)は(3 / (2π)^3) * integrand(x)を積分したもの
     cuba_func(x, f) = (f[1] = integrand(x) * 3 / (2π)^3; return 0)
 
     result = cuhre(cuba_func, 3, 1; rtol=1e-6)
@@ -26,6 +27,7 @@ function u3_cuba()
     return val, err
 end
 
+# p(3) ≡ 1 - 1 / u(3)を計算する
 function p_3()
     val, err = u3_cuba()
     value = 1 - 1 / val
